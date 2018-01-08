f3c94ca34d7fbb947de6420302e968b343bd76c7
Unknown <bolmier.geoffrey@orange.fr>
2018-01-08 13:23:17 +0100

    INIT liste_categorie_stage.py

---
+++ liste_categorie_stage.py
@@ -0,0 +1,4 @@
+classes_theme = ['economie', 'afrique', 'asie pacifique', 'voyage et sortie',
+'littérature', 'musique', 'automobile', 'ameriques', 'europe', 'ecole', 'science',
+'politique fr', 'jeux video', 'proche orient', 'sante', 'gastronomie',
+'sport', 'audiovisuel', 'art et culture']
