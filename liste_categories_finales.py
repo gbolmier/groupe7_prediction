c061256d4bc4b615761933125ab8b60044935e2f
Unknown <bolmier.geoffrey@orange.fr>
2018-01-09 10:14:06 +0100

    UPDATE liste_categories
    RENAME liste_categorie_stage.py : liste_categories_finales.py

---
+++ liste_categories_finales.py
@@ -0,0 +1,18 @@
+# Catégories finales
+categories = [
+        'international', # politique etrangere, voyage
+        'politique fr', 
+        'france', # actu fait divers en france NON POLITIQUE
+        'economie', 
+        'science/high-tech', # jeux video
+        'art et culture', # gastronomie, mode, arts, cinema, voyages, litterature
+        'sport', # automobile
+        'sante'
+        ]
+
+
+# Catégories du stage
+classes_theme = ['economie', 'afrique', 'asie pacifique', 'voyage et sortie',
+'littÃ©rature', 'musique', 'automobile', 'ameriques', 'europe', 'ecole', 'science',
+'politique fr', 'jeux video', 'proche orient', 'sante', 'gastronomie',
+'sport', 'audiovisuel', 'art et culture']
