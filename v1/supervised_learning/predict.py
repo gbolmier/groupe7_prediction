b34753979c90eaaa3c01b1648cace71740a922b6
Unknown <plissonneau.antoine@gmail.com>
2018-01-12 09:48:18 +0100

    predict

--- v1/supervised_learning/predict.py
+++ v1/supervised_learning/predict.py
@@ -1,9 +1,54 @@
 # -*- coding: utf-8 -*-
 # group 7
 
-import requests
 import pickle
+import pandas as pd
+import json
 
 
 # Import our classifier and articles to predict
-clf = pickle.load(open('clf', 'rb'))
+model = pickle.load(open('clf', 'rb'))
+
+test = pickle.load(open('../pre_processing/recoded_df', 'rb'))
+vectorizer = pickle.load(open('vectorizer', 'rb'))
+
+#test = test[test['theme'] == 'delete']
+
+test_vec = vectorizer.transform(test['list_lemma']).toarray()
+
+
+predicted = model.predict(test_vec)
+#predicted = predicted.argmax(1) #Proba to selected label
+
+dict_inv_classes = {
+            0:'international',
+            1:'france',
+            2:'economie',
+            3:'sciences_high_tech',
+            4:'arts_et_culture',
+            5:'sports',
+            6:'sante'
+        }
+cat_pred = pd.Series(predicted).map(dict_inv_classes)
+
+
+res = [
+       
+       { str(i): j} for (i,j) in zip(test['id'],cat_pred)  
+       
+       ]
+
+
+
+#import json
+#
+#for js in res:
+#    print(js)
+#    with open('a' + '.json', 'w') as fp:
+#        json.dump(js, fp)   
+#        
+#        
+
+res = [{1:'sante'}, {2:'sciences_high_tech'}]
+import requests
+r = requests.post('http:///post', data = res)
